c4b38965ed9682f277a89537aebecd67bd0f092a
Nonso-Marshal <anawanacu@gmail.com>
2025-09-29 14:37:47 +0100

    updated the login endpoint on auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from dependency import verify_password, create_access_token, create_refresh_token, get_current_user
 from crud.user import UserService
-from schemas.auth import UserRegister, UserLogin, Token
+from schemas.auth import UserRegister, Token
 from models import User as DBUser
 from schemas.user import UserResponse
 from database import get_db
@@ -38,16 +38,16 @@ def register(user_data: UserRegister, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Registration failed")
 
 @auth_router.post("/login", response_model=Token, status_code=status.HTTP_200_OK)
-def login(user_data: UserLogin, db: Session = Depends(get_db)):
-    logger.debug(f"Login attempt for email: {user_data.email}")
-    logger.debug(f"Received payload: {user_data.dict()}")
-    user = db.query(DBUser).filter(DBUser.email == user_data.email).first()
-    if not user or not verify_password(user_data.password, user.password_hash):
-        logger.error(f"Login failed for {user_data.email}: Invalid credentials")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    logger.debug(f"Login attempt for email: {form_data.username}")
+    logger.debug(f"Received payload: {form_data.__dict__}")
+    user = db.query(DBUser).filter(DBUser.email == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.password_hash):
+        logger.error(f"Login failed for {form_data.username}: Invalid credentials")
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
     access_token = create_access_token({"sub": str(user.id), "role": user.role.value})
     refresh_token = create_refresh_token({"sub": str(user.id), "role": user.role.value})
-    logger.info(f"Login successful for {user_data.email}")
+    logger.info(f"Login successful for {form_data.username}")
     return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
 
 @auth_router.post("/refresh", response_model=Token, status_code=status.HTTP_200_OK)
